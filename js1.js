602008ec26bf3d9ab9f1efe79f89ecba9cf1d059
nivedlal <nivedlalp@gmail.com>
2022-09-13 14:09:48 +0530

    day1

---
+++ js1.js
@@ -0,0 +1,51 @@
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+
+var myName;
+
+var a;
+a = 7;
+var b;
+b=a;
+
+var myFirstName = "nived";
+var myLastName = "lal";
+
+var x=5;
+var y=10;
+var z="I am a";
+x = x + 1;
+y = y + 5;
+z = z + " String!";
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact);
+
+let myVar = 87;
+myVar = ++myVar;
+
+const some = "I am a \"double quoted\" string inside \"double quotes\"."; 
+const string = '<a href="http://www.example.com" target="_blank">Link</a>';
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+const myStr1 = "This is the start."+" This is the end."; 
+let myStr2="This is the first sentence.";
+myStr2+= " This is the second sentence.";
+const myName = "freeCodeCamp";
+const myStr3 = "My name is "+myName+"and I am well!";
+const someAdjective = "awesome";
+let myStr4 = "Learning to code is ";
+myStr4+= someAdjective;
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+lastNameLength = lastName.length;
+let firstLetterOfLastName1 = "";
+const lastName1 = "Lovelace";
+firstLetterOfLastName = lastName1[0];
+
